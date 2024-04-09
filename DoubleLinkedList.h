ca6d686cf21d413ea352ec707a95efcbd4b50ad6
jehoon5940 <128759974+jehoon5940@users.noreply.github.com>
2024-04-09 18:49:20 +0900

    Create DoubleLinkedList.h

---
+++ DoubleLinkedList.h
@@ -0,0 +1,18 @@
+#pragma once
+// 이중 연결 리스트의 노드 구조를 구조체로 정의
+typedef struct List Node {
+struct ListNode* link;
+char data[4];
+struct ListNode* link; 
+} listNode;
+
+// 리스트 시작을 나타내는 head 노드를 구조체로 정의 
+typedef struct {
+listNode* head;
+} linkedList_h;
+
+linkedlist_h* createLinkedList_h(void); 
+void printList(linkedList_h* DL);
+void insertNode(linkedList_h* DL. listNode* pre, char* x); 
+void deleteNode(linkedList_h* DL, listNode* old);
+listNode* searchNode(linkedList_h* DL, char* x);
